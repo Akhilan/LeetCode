3b35c4dd8c9e58c5d9c9c4ee1a2c102730a35954
Akhilan <akhilkrishnans@gmail.com>
2024-01-31 16:01:17 +0530

    Time: 39 ms (34.44%), Space: 18.8 MB (90.26%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -10,10 +10,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        slow = fast = head
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            if slow == fast:
-                return True
-        return False
+        try:
+            slow = head
+            fast = head.next
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
