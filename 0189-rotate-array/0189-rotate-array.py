2610a85c3322ca92b6cc4f97807baee4a8f4fb12
Akhilan <akhilkrishnans@gmail.com>
2024-02-06 15:44:58 +0530

    Time: 141 ms (94.67%), Space: 23.2 MB (90.37%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,22 +5,6 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
-        def reverse(nums, start, end):
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-        
-        k %= len(nums)
-    
-        # Reverse the entire array
-        reverse(nums, 0, len(nums) - 1)
-
-        # Reverse the first k elements
-        reverse(nums, 0, k - 1)
-
-        # Reverse the remaining elements
-        reverse(nums, k, len(nums) - 1)
-
-    
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[n-k:] + nums[:n-k]
