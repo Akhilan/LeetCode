175d8f8eac3f3d53e2bcb9f79c8c345e64b08c59
Akhilan <akhilkrishnans@gmail.com>
2024-03-13 21:58:26 +0530

    Time: 12 ms (90.85%), Space: 11.5 MB (96.73%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def pivotInteger(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        right_sum = n * (n + 1) // 2
+        left_sum = 0
+        for i in range(1, n + 1):
+            right_sum -= i
+            if left_sum == right_sum:
+                return i
+            left_sum += i
+        return -1
